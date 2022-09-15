f5c091023c1a9dcf0a97e031cc516061455d0224
AndresZea2003 <afzea2003@gmail.com>
2022-09-15 00:30:33 -0500

    feat: Vue.js installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import {createApp} from "vue/dist/vue.esm-bundler.js";
+import CButton from "./components/CButton.vue"
+
+createApp({
+    components: {
+        'c-button': CButton,
+    }
+}).mount("#app");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="bg-red-400">
+    <div class="bg-red-400" id="app">
         Hola
+        <c-button></c-button>
     </div>
 </body>
 </html>
